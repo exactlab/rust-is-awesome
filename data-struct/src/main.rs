a076c9fb50f319a975319948042ccd6ceebea425
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-11-10 15:26:10 +0100

    create empty vector

--- data-struct/src/main.rs
+++ data-struct/src/main.rs
@@ -1,3 +1,24 @@
+extern crate libc; // defined in .toml
+use std::mem;
+
+#[derive(Debug)]
+struct Vector<T: Copy> {
+    len: u32,
+    start: *mut T,
+}
+
+impl<T: Copy> Vector<T>{
+    fn new() -> Self {
+        unsafe {
+            Self { 
+                len: 0,
+                start: libc::malloc(1*mem::size_of::<T>()) as *mut T
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let vector = Vector::<i32>::new();
+    println!("{:?}", vector)
 }
